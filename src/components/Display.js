9e93061a3c6832163c21f3125c4007cfef074ad1
Davi Santos <daavsantosl@gmail.com>
2022-07-06 21:25:28 -0300

    refactor: state props moved to a destructure variables.

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,10 +3,12 @@ import React from 'react';
 
 export default class Display extends React.Component {
   render() {
+    const {operation, result} = this.props.appState
+    
     return (
       <div className="Display">
-        <h1 className="typedOperation">{this.props.operationState.operation}</h1>
-        <h2 className="resultPreview">{this.props.operationState.result}</h2>
+        <h1 className="typedOperation">{operation}</h1>
+        <h2 className="resultPreview">{result}</h2>
       </div>
     )
   }
