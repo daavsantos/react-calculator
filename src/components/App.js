d92c0439fca0122ff1def22e7bff36dd7013c4ea
Davi Santos <daavsantosl@gmail.com>
2022-07-06 21:52:17 -0300

    refactor: all Key related methods moved to Keys component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from 'react';
-import { evaluate } from 'mathjs'
 import Display from './Display'
 import Keys from './Keys'
 
@@ -11,45 +10,13 @@ class App extends React.Component {
       operation: "",
       result: ""
     }
-    this.handleKey = this.handleKey.bind(this)
-    this.tryOperation = this.tryOperation.bind(this)
-  }
-
-  tryOperation(operation) {
-    try { return evaluate(operation) } catch { return this.state.result }
-  }
-  
-  handleKey(key) {
-    const maxOperationSize = 12
-    switch (key) {
-      case "DEL":
-        this.setState({ operation: "", result: "" })
-        break;
-      case "=":
-        this.setState((lastOperation) => ({
-          operation: this.tryOperation(lastOperation.operation),
-          result: ""
-        }))
-        break;
-      case "<":
-        { /* Arrow menu soon.. */ }
-        break;
-      default:
-        const operationSize = this.state.operation.length
-        if (operationSize < maxOperationSize || !operationSize) {
-          this.setState((lastOperation) => ({
-            operation: lastOperation.operation + key,
-            result: this.tryOperation(lastOperation.operation + key)
-          }))
-        }
-    }
   }
 
   render() {
     return (
       <div className="App">
         <Display appState={this.state} />
-        <Keys handleKey={this.handleKey} />
+        <Keys handleKey={this.handleKey} appState={this.state} setAppState={(props) => this.setState({...props})} />
       </div>
     )
   }
--- src/components/Keys.js
+++ src/components/Keys.js
@@ -1,5 +1,6 @@
 import './Keys.css'
 import React from "react"
+import { evaluate } from 'mathjs'
 
 class Key extends React.Component {
   render() {
@@ -31,6 +32,38 @@ export default class Keys extends React.Component {
     super(props)
     this.numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", ".", "="]
     this.operators = ["DEL", "/", "*", "-", "+"]
+    this.handleKey = this.handleKey.bind(this)
+    this.tryOperation = this.tryOperation.bind(this)
+  }
+
+  tryOperation(operation) {
+    try { return evaluate(operation) } catch { return this.props.appState.result }
+  }
+
+  handleKey(key) {
+    const maxOperationSize = 12
+    switch (key) {
+      case "DEL":
+        this.props.setAppState({ operation: "", result: "" })
+        break;
+      case "=":
+        this.props.setAppState({
+          operation: this.tryOperation(this.props.appState.operation),
+          result: ""
+        })
+        break;
+      case "<":
+        { /* Arrow menu soon.. */ }
+        break;
+      default:
+        const operationSize = this.props.appState.operation.length
+        if (operationSize < maxOperationSize || !operationSize) {
+          this.props.setAppState({
+            operation: this.props.appState.operation + key,
+            result: this.tryOperation(this.props.appState.operation + key)
+          })
+        }
+    }
   }
 
   render() {
@@ -38,16 +71,16 @@ export default class Keys extends React.Component {
       <div className="Keys">
         <div className="Numbers">
           {this.numbers.map((number) =>
-            <Key key={"KTR" + number} handleKey={this.props.handleKey} keyClass="numKey" symbol={number}></Key>
+            <Key key={"KTR" + number} handleKey={this.handleKey} keyClass="numKey" symbol={number}></Key>
           )}
         </div>
         <div className="Operators">
           {this.operators.map((operator) =>
-            <Key key={"KTR" + operator} handleKey={this.props.handleKey} keyClass="operatorKey" symbol={operator}></Key>
+            <Key key={"KTR" + operator} handleKey={this.handleKey} keyClass="operatorKey" symbol={operator}></Key>
           )}
         </div>
         <div className="ArrowMenu">
-          <Key key={"KTR<"} handleKey={this.props.handleKey} keyClass="arrowKey" symbol="<" />
+          <Key key={"KTR<"} handleKey={this.handleKey} keyClass="arrowKey" symbol="<" />
         </div>
       </div>
     )
